8aee7703ed1e417477d32cc7eaee739e5a26eb6d
Sarah Anja <100360306+macobac@users.noreply.github.com>
2023-05-14 15:49:15 +0200

    idk js

--- js/katter.js
+++ js/katter.js
@@ -1,18 +1,39 @@
 const POSTS_API = "http://kattens-vern.local/wp-json/wp/v2/posts";
 
-let katterDiv = document.querySelector(".katter-cat-container");
-
-fetch (POSTS_API)
+fetch(POSTS_API)
     .then(res => res.json())
     .then(data => {
-        data.forEach(post => {
-            const postCard = document.createElement("div");
-            postCard.classList.add("post-card");
-            postCard.innerHTML = `
-                <h1>${post.title.rendered}</h1>
+        const catNames = data.map(post => {
+            const splits = post.title.rendered.split(" ");
+            return splits[0];
+        });
+
+        const catAges = data.map(post => {
+            const splits = post.title.rendered.split(" ");
+            const catAge = splits.slice(1).join(" ");
+            return catAge;
+        });
+
+        const catImgs = data.map(post => {
+            const content = post.content.rendered;
+            const regex = /<img.*?src="(.*?)"/;
+            const match = content.match(regex);
+            return match ? match[1] : null;
+        });
+
+        data.forEach((post, index) => {
+            const html = `
+              <div class="post">
+              <img src="${catImgs[index]}">
+                <h2>${catNames[index]}</h2>
+                <p>${catAges[index]}</p>
+              </div>
             `;
-            
-        katterDiv.appendChild(postCard);
-    });
-})
+            document.querySelector(".katter-cat-container").innerHTML += html;
+        });
+    })
     .catch(error => console.error(error));
+
+//slice img og store i en ny variable som kan ha forskjellig value ettersom hvilken katt det er. valuen må settes i en loop i function, men declaration kan skje utenom da vi trenger å style det?
+
+//slice første ordet av tittelen til å bli egen variabel, resten av tittelen til å bli egene variabel
