8e8a7af128ca0f50a8146ae074d97b026c048c80
Sarah Anja <100360306+macobac@users.noreply.github.com>
2023-05-18 13:28:20 +0200

    index.js

---
+++ js/index.js
@@ -0,0 +1,56 @@
+const POSTS_API = "http://kattens-vern.local/wp-json/wp/v2/posts";
+const catContainer = document.querySelector(".homepg-cat-container");
+let page = 1;
+let limit = 4;
+let totalPosts = 0;
+
+function fetchAPI() {
+
+    fetch(POSTS_API + "?page=" + page + "&per_page=" + limit)
+        .then((res) => res.json())
+        .then((data) => {
+            totalPosts = data.length;
+            displayPosts(data);
+            page++;
+        })
+        .catch((error) => console.error(error));
+}
+
+fetchAPI();
+
+function displayPosts(data) {
+
+    const catNames = data.map((firstWord) => {
+        const firstWords = firstWord.title.rendered.split(" ");
+        return firstWords[0];
+    });
+
+    const catAges = data.map((restWord) => {
+        const restWords = restWord.title.rendered.split(" ");
+        const lastWord = restWords.pop();
+        const secLastWord = restWords.pop();
+        const joinedWords = secLastWord + " " + lastWord;
+        return joinedWords;
+    });
+
+    const catImgs = data.map((image) => {
+        const tempDiv = document.createElement('div');
+        tempDiv.innerHTML = image.content.rendered;
+        const imgEl = tempDiv.querySelector('img');
+        const src = imgEl ? imgEl.getAttribute('src') : null;
+        return src;
+    });
+
+    data.forEach((post, i) => {
+        const postBoxes = `
+        <a href="/html/katt-spes.html?id=${post.id}">
+        <div class="cat-posts">
+          <img src="${catImgs[i]}">
+          <h2 class="catName">${catNames[i]}</h2>
+          <p class="catAge">${catAges[i]}</p>
+        `
+        catContainer.innerHTML += postBoxes;
+    });
+
+};
+
