55aaab321f4ab9927e8e78aab8a2f1982934ca25
Sarah Anja <100360306+macobac@users.noreply.github.com>
2023-05-24 13:43:08 +0200

    idk what im doing

--- js/katt-spes.js
+++ js/katt-spes.js
@@ -13,30 +13,21 @@ function fetchAPI() {
         .catch((error) => console.error(error));
 };
 
-function catImgModal(e) {
-    const modalContainer = document.querySelector(".cat-posts-img #modalContainer");
-
-    if (e.target.tagName === "IMG") {
-        modalContainer.classList.add("open-modal");
-    } else {
-        modalContainer.classList.remove("open-modal");
-    }
-}
-
 function displayPost(data) {
 
     const catName = data.title.rendered.split(" ")[0];
 
-    const tempDiv = document.createElement('div');
+    const tempDiv = document.createElement("div");
     tempDiv.innerHTML = data.content.rendered;
-    const imgEl = tempDiv.querySelector('img');
-    const src = imgEl ? imgEl.getAttribute('src') : null;
+    const imgEl = tempDiv.querySelector("img");
+    const src = imgEl ? imgEl.getAttribute("src") : null;
 
     const ps = tempDiv.getElementsByTagName("p");
     const pText = Array.from(ps).map(p => p.innerHTML).join(" ");
 
     const fetchedPost = `
-        <h1 class="katt-spes-title normal-heading">Katter som trenger hjem - ${catName}</h1>
+        <h1 class="katt-spes-title normal-heading">
+        Katter som trenger hjem - ${catName}</h1>
         <div class="cat-posts-img katt-spes-img">
         <img src="${src}">
         <div id="modalContainer"></div>
@@ -47,20 +38,14 @@ function displayPost(data) {
 
     spesContainer.innerHTML += fetchedPost;
 
-
-    const catPostsModal = spesContainer.querySelector(".cat-posts-img");
-    if (catPostsModal) {
-        catPostsModal.addEventListener("click", catImgModal);
-    }
-
     const catgArr = data.categories;
     const catgProm = catgArr.map((catgId) => getCategory(catgId));
 
     Promise.all(catgProm)
         .then((catgs) => {
             const fetchedCatgs = `
-  <div class="catg-wrapper">
-            ${catgs.map((catg) => `
+                <div class="catg-wrapper">
+                    ${catgs.map((catg) => `
                 <div class="catpaw-symbol"></div>
                 <div class="categories">
                     <p>${catg}</p>
@@ -84,4 +69,24 @@ function getCategory(catgId) {
 };
 
 
-document.addEventListener("DOMContentLoaded", fetchAPI);
+
+fetchAPI();
+
+
+
+
+/* document.addEventListener("DOMContentLoaded", fetchAPI);
+function catImgModal(e) {
+    const modalContainer = document.querySelector(".cat-posts-img #modalContainer");
+
+    if (e.target.tagName === "IMG") {
+        modalContainer.classList.add("open-modal");
+    } else {
+        modalContainer.classList.remove("open-modal");
+    }
+}
+const catPostsModal = spesContainer.querySelector(".cat-posts-img");
+if (catPostsModal) {
+    catPostsModal.addEventListener("click", catImgModal);
+}
+ */
